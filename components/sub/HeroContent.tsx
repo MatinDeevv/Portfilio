00c539250a0d5134b6a6795c6c9c227687ca03ac
MatinDev <business.MatinDev@gmail.com>
2024-12-12 19:20:56 -0500

    Logo styles fixed

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -24,7 +24,7 @@ const HeroContent = () => {
         >
           <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
           <h1 className="Welcome-text text-[13px]">
-            Fullstack Developer Portfolio
+            Fullstack Developer
           </h1>
         </motion.div>
 
@@ -34,7 +34,7 @@ const HeroContent = () => {
         >
           <span>
             Hello,My name is
-            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-700 to-cyan-500">
               {" "}
              Matin{" "}
             </span>
