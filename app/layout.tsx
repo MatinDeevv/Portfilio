7dfd527d634ee0524689e8a6c5f096a54c327499
MatinDev <business.MatinDev@gmail.com>
2024-12-12 19:24:02 -0500

    Resolved merge conflicts agiann

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,8 @@ import Footer from "@/components/main/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Space Portfolio",
-  description: "This is my portfolio",
+  title: "MatinDev",
+  description: "This is my portfilio",
 };
 
 export default function RootLayout({
